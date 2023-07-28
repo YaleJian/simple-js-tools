28c6479ce6a793a6ff7b1673dba2ecaab3e0624f
Yale Jian <yalejian@gmail.com>
2023-07-28 14:59:28 +0800

    feat：docs

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
     url: 'https://yalejian.github.io',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/yale-js-tools-doc',
+    baseUrl: '/yale-js-tools',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
