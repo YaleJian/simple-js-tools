16bd81a963fc942384a475c6f750804922505b9d
Yale Jian <yalejian@gmail.com>
2023-07-28 11:06:46 +0800

    feat：docs

---
+++ docs/docusaurus.config.js
@@ -0,0 +1,190 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+    title: 'Yale JS Tools',
+    tagline: '轻量的Javascript工具包',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://yalejian.github.io',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/yale-js-tools-doc',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'yalejian', // Usually your GitHub org/user name.
+    projectName: 'yale-js-tools-doc', // Usually your repo name.
+    deploymentBranch: 'gh-pages',
+    trailingSlash: false,
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internalization, you can use this field to set useful
+    // metadata like html lang. For example, if your site is Chinese, you may want
+    // to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'zh',
+        locales: ['zh'],
+    },
+    plugins: [require.resolve('docusaurus-lunr-search')],
+
+    presets: [
+        //预设
+        [
+            'classic',
+            /** @type {import('@docusaurus/preset-classic').Options} */
+            ({
+                docs: {
+                    path:"document",
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/YaleJian/yale-js-tools-doc',
+                },
+                blog: {
+                    showReadingTime: true,
+                    // Please change this to your repo.
+                    // Remove this to remove the "edit this page" links.
+                    editUrl:
+                        'https://github.com/YaleJian/yale-js-tools-doc',
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
+            }),
+        ],
+    ],
+
+    themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+        ({
+            // Replace with your project's social card
+            image: 'img/docusaurus-social-card.jpg',//元标签的默认图像
+            colorMode: {
+                defaultMode: 'light',
+                disableSwitch: false,
+                respectPrefersColorScheme: true,
+            },
+            announcementBar: {
+                id: 'support_us',
+                content:
+                    '编码不易，给个星星支持下吧 <a target="_blank" rel="noopener noreferrer" href="https://github.com/YaleJian/yale-js-tools">GitHub</a>',
+                backgroundColor: '#fafbfc',
+                textColor: '#091E42',
+                isCloseable: false,
+            },
+            tableOfContents: {
+                minHeadingLevel: 2,
+                maxHeadingLevel: 6,
+            },
+            navbar: {
+                title: 'Yale JS Tools',
+                logo: {
+                    alt: 'Yale JS Tools',
+                    src: 'img/logo.svg',
+                    srcDark: 'img/logo_dark.svg',
+                    href: '/',
+                    target: '_self',
+                    width: 32,
+                    height: 32,
+                    className: 'custom-navbar-logo-class',
+                    style: {border: 'white'},
+                },
+                hideOnScroll: true,//滚动自动隐藏导航栏
+                items: [
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'tutorialSidebar',
+                        position: 'left',
+                        label: 'Tutorial',
+                    },
+                    {
+                        type: 'docSidebar',
+                        sidebarId: 'apiSidebar',
+                        position: 'left',
+                        label: 'API',
+                    },
+                    {to: '/blog', label: '更新日志', position: 'left'},
+                    {
+                        type: 'search',
+                        position: 'left',
+                    },
+                    {
+                        type: 'docsVersionDropdown',
+                        position: 'right',
+                        dropdownItemsAfter: [{to: '/docs/API/intro', label: '1.0.0-beta.2'}],
+                        dropdownActiveClassDisabled: false,
+                    },
+                    {
+                        type: 'localeDropdown',
+                        position: 'right',
+                    },
+                    {
+                        href: 'https://github.com/facebook/docusaurus',
+                        label: 'GitHub',
+                        position: 'right',
+                    },
+                ],
+            },
+            footer: {
+                style: 'dark',
+                links: [
+                    {
+                        title: 'Docs',
+                        items: [
+                            {
+                                label: 'Tutorial',
+                                to: '/docs/intro',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'Community',
+                        items: [
+                            {
+                                label: 'Stack Overflow',
+                                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                            },
+                            {
+                                label: 'Discord',
+                                href: 'https://discordapp.com/invite/docusaurus',
+                            },
+                            {
+                                label: 'Twitter',
+                                href: 'https://twitter.com/docusaurus',
+                            },
+                        ],
+                    },
+                    {
+                        title: 'More',
+                        items: [
+                            {
+                                label: 'Blog',
+                                to: '/blog',
+                            },
+                            {
+                                label: 'GitHub',
+                                href: 'https://github.com/facebook/docusaurus',
+                            },
+                        ],
+                    },
+                ],
+                copyright: `Copyright © ${new Date().getFullYear()} Yale Jian`,
+            },
+            prism: {
+                //Docusaurus使用Prism React Renderer来突出显示代码块
+                theme: lightCodeTheme,
+                darkTheme: darkCodeTheme,
+            },
+        }),
+};
+
+module.exports = config;
